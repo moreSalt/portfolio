0453c0c57110b0a98beec5d69ce9a2049169cff2
Fran <mildmoose@pm.me>
2022-11-05 21:48:20 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfolio
+
+I create stuff
---
+++ src/routes/+page.svelte
@@ -0,0 +1,133 @@
+<div class="navbar bg-base-100">
+    <div class="navbar-start">
+      <div class="dropdown">
+        <label tabindex="0" class="btn btn-ghost btn-circle">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+        </label>
+        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+          <li><a href="https://github.com/moreSalt" target="_blank"><i class="fa-brands fa-github"></i> Github</a></li>
+          <li><a href="https://gitlab.com/bannedSubnet" target="_blank"><i class="fa-brands fa-gitlab"></i> Gitlab</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="navbar-end">
+      <a class="btn btn-ghost btn-circle" href="mailto:mildmoose@pm.me" target="_blank">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" /></svg>
+    </a>
+    </div>
+</div>
+
+<main class="w-full flex flex-col items-center gap-8">
+    <h1 class="text-5xl font-extrabold">Hi I'm <span class="text-transparent bg-clip-text bg-gradient-to-br from-success to-info">Paul</span></h1>
+
+    <p>Fullstack developer in Boulder.</p>
+
+
+    <div class="grid grid-rows-fr grid-cols-1  md:grid-cols-2 lg-grid-cols-4 w-2/3 border shadow-xl bg-base-200 p-2 rounded gap-4">
+        <div class="flex gap-2 items-center justify-center">
+            <i class="fa-brands fa-js"></i>
+            JS/TS
+        </div>
+        <div class="flex gap-2 items-center justify-center">
+            <i class="fa-brands fa-js"></i>
+            Svelte/Sveltekit
+        </div>
+        <div class="flex gap-2 items-center justify-center">
+            <i class="fa-brands fa-node-js"></i>
+            Node.js
+        </div>
+        <div class="flex gap-2 items-center justify-center">
+            <i class="fa-brands fa-python"></i>
+            Python
+        </div>
+        <div class="flex gap-2 items-center justify-center">
+            <i class="fa-solid fa-c"></i>
+            C++
+        </div>
+
+        <div class="flex gap-2 items-center justify-center">
+            <i class="fa-brands fa-golang"></i>
+            Go
+        </div>
+
+        <div class="flex gap-2 items-center justify-center">
+            <i class="fa-brands fa-html5"></i>
+            HTML/CSS
+        </div>
+        <div class="flex gap-2 items-center justify-center">
+            <i class="fa-brands fa-css3"></i>
+            Tailwind CSS
+        </div>
+        <div class="flex gap-2 items-center justify-center">
+            <i class="fa-brands fa-git-alt"></i>
+            Git
+        </div>
+        <div class="flex gap-2 items-center justify-center">
+            <i class="fa-brands fa-aws"></i>
+            AWS
+        </div>
+        <div class="flex gap-2 items-center justify-center">
+            <i class="fa-solid fa-server"></i>
+            Supabase
+        </div>
+        <div class="flex gap-2 items-center justify-center">
+            <i class="fa-solid fa-database"></i>
+            Mongo, Postgres, Redis
+        </div>
+    </div>
+
+
+    <div class="grid grid-cols-1 gap-4 sm:gap-0 sm:grid-cols-2 w-2/3">
+        
+        <a class="border p-4 w-full gap-2" href="./" target="_blank" rel="noreferrer">
+            <h2 class="card-title">
+                <i class="fa-solid fa-briefcase"></i>
+                  
+                Software Developer
+            </h2>
+            <p class="text-slate-400 text-sm">May 2021 - Present</p>
+            <p>I am a full-stack developer for a desktop applicaiton and Chrome extension. Technologies used include TS, Svelte, Redis, AWS, and Node.js</p>
+
+        </a>
+
+        <a class="border p-4 w-full gap-2" href="https://github.com/moreSalt/portfolio" target="_blank" rel="noreferrer">
+            <h2 class="card-title">
+                <i class="fa-solid fa-user"></i>
+                Personal portfolio
+            </h2>
+            <p class="text-slate-400 text-sm">Nov 2022</p>
+            <p>You are here, a small site about me</p>
+
+        </a>
+
+        <a class="border p-4 w-full gap-2" href="https://github.com/moreSalt/calen-duh" target="_blank" rel="noreferrer">
+            <h2 class="card-title">
+                <i class="fa-solid fa-user"></i>
+                Calenduh
+            </h2>
+            <p class="text-slate-400 text-sm">Oct 2022</p>
+            <p>A simple web calendar I made to try out Sveltekit and Supabase</p>
+
+        </a>
+
+        <a class="border p-4 w-full gap-2" href="https://github.com/moreSalt/hellofresh-instacart" target="_blank" rel="noreferrer">
+            <h2 class="card-title">
+                <i class="fa-solid fa-user"></i>
+                HelloFresh to Instacart
+            </h2>
+            <p class="text-slate-400 text-sm">Oct 2022</p>
+            <p>A script to easily find HelloFresh ingredients in Instacart</p>
+
+        </a>
+
+        <a class="border p-4 w-full gap-2 col-span-full" href="./" target="_blank" rel="noreferrer">
+            <h2 class="card-title">
+                <i class="fa-solid fa-user"></i>
+                +more
+            </h2>
+            <p class="text-slate-400 text-sm">August 2021 - Present</p>
+            <p>Want to hear about some of my private projects, just shoot me an email.</p>
+
+        </a>
+    </div>
+</main>
