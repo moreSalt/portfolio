807c03ae6663d3e5245fec89dc4e06f9cea1235a
Paul Rodriguez <83256976+moreSalt@users.noreply.github.com>
2024-10-11 20:48:22 -0600

    i am stupid

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,9 @@
 <script>
-    import "../app.css";
+	import '../app.css';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
+
 <slot />
